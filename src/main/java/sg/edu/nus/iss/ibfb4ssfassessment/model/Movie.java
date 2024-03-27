fd41074847322fb07dce8a45897b367acf6f6fe3
al-uminium <aeronafic@gmail.com>
2024-03-27 09:24:22 +0800

    initialized repo

---
+++ src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/model/Movie.java
@@ -0,0 +1,15 @@
+package sg.edu.nus.iss.ibfb4ssfassessment.model;
+
+
+public class Movie {
+  private Integer movieId;
+  private String title; 
+  private String year;
+  private String rated;
+  private Long releaseDate;
+  private String runTime;
+  private String genre;
+  private String director;
+  private Double rating; 
+  
+}
