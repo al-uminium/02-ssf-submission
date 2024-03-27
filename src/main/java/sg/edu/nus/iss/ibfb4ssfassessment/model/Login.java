d089c916eff2b3d097f804984d24d50addf06f57
al-uminium <aeronafic@gmail.com>
2024-03-27 09:54:48 +0800

    initialized login and movie models

--- src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/model/Login.java
+++ src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/model/Login.java
@@ -1,8 +1,21 @@
 package sg.edu.nus.iss.ibfb4ssfassessment.model;
 
+import java.util.Date;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Past;
 
 public class Login {
+  @Email
+  @NotEmpty
+  @Max(50)
+  private String email;
 
+  @Past
+  //! TODO add annotation for  format
+  private Date birthdate;
 
     
 }
--- src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/service/FileService.java
+++ src/main/java/sg/edu/nus/iss/ibfb4ssfassessment/service/FileService.java
@@ -1,6 +1,10 @@
 package sg.edu.nus.iss.ibfb4ssfassessment.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import sg.edu.nus.iss.ibfb4ssfassessment.model.Movie;
 
 public record FileService() {
 
